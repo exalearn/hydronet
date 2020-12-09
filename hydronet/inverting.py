307d1be3b0451d4b8f376c5549f892adba4c926a
Logan Ward <ward.logan.t@gmail.com>
2020-12-09 16:37:27 +0000

    Initial notebook for graph-to-xyz

---
+++ hydronet/inverting.py
@@ -0,0 +1,147 @@
+"""Functions for inverting a structure from graph to coordinates"""
+
+import networkx as nx
+import numpy as np
+import ase
+
+_h20_bond_angle = 2 * np.pi * 104.5 / 360
+
+
+def convert_directed_graph_to_xyz(graph: nx.DiGraph, n_h_guesses: int = 5) -> ase.Atoms:
+    """Generate initial coordinates for water cluster from the directed graph of bonded waters
+
+    Args:
+        graph: Directed graph of how waters are bonded
+        n_h_guesses: Number of guesses to make for each hydrogen position.
+            We select the one that is farthest from all other atoms
+    Returns:
+        Atoms object describing candidate position of hydrogens
+    """
+    
+    # Generate coordinates using the spring model
+    pos = nx.spring_layout(graph.to_undirected(), iterations=200, dim=3, center=(0, 0, 0))
+    
+    # Make the coordinates into a numpy array
+    pos = np.array([pos[i] for i in range(len(pos))])  # From a dict
+    
+    # Expand so that most bonds are longer than 2.9 A
+    #  TODO: (wardlt) Make this bond distance configurable
+    bond_lengths = []
+    for a, b in graph.edges:
+        bond_lengths.append(np.linalg.norm(pos[a] - pos[b]))
+    scale_factor = 2.9 / np.percentile(bond_lengths, 25)
+    pos *= scale_factor
+    
+    # Make a map of oxygen index to covalently-bonded hydrogens
+    h_map = [[] for _ in range(pos.shape[0])]
+    h_count = 0
+    
+    # Place hydrogens 0.9641 Ang along donated bond
+    h_pos = []
+    for a, b, t in graph.edges(data='label'):
+        if t == 'accept': 
+            continue
+        
+        # Compute position along A->B path
+        vec = pos[b] - pos[a]
+        t = 0.9641 / np.linalg.norm(vec)
+        
+        # Clip it so that it is at least as close
+        #  to the oxygen with the covalent bond
+        t = min(0.5, t)
+        h_pos.append(pos[a] + t * vec)
+        
+        # Associate this hydrogen with the oxygen
+        h_map[a].append(h_count)
+        h_count += 1
+    pos = np.vstack((pos, h_pos))
+    
+    # Place remaining hydrogens
+    for oxy in graph.nodes():
+        # Determine the edges associated with donated hydrogen bonds
+        #  Hydrogens for these are already placed
+        my_h = [e for e in graph[oxy] if graph[oxy][e]['label'] == 'donate']
+        
+        # If you have two hydrogens, no more to place
+        if len(my_h) == 2:
+            # TODO (wardlt): Force the angle to be ~106.5
+            continue
+        
+        # If you have none, first place one randomly
+        #  Use Gaussian-distributed points to generate random points
+        #  https://mathworld.wolfram.com/SpherePointPicking.html
+        if len(my_h) == 0:
+            new_vecs = np.random.normal(size=(n_h_guesses, 3))
+            new_vecs = 0.9641 / np.linalg.norm(new_vecs, axis=1)[:, None] * new_vecs
+            possible_placements = new_vecs + pos[oxy]
+            
+            # Determine distances 
+            #  Shape is: possible_placements x current_atom_count
+            all_dists = np.linalg.norm(pos[None, :, :] - possible_placements[:, None, :], axis=-1)
+            closest_dist = np.min(all_dists, axis=1)  # Shape: possible_placements
+            
+            # Find the farthest atom
+            best_H = np.argmax(closest_dist)
+            
+            # Get the direction to the hydrogen we picked
+            h1_dir = new_vecs[best_H, :]
+            
+            # Add it to the list of atoms
+            #  TODO (wardlt): Is continually re-allocating the array an performance issue?
+            pos = np.vstack((pos, possible_placements[best_H, :]))
+            
+            # Associate the hydrogen with the oxygen
+            h_map[oxy].append(h_count)
+            h_count += 1
+            
+        # If you have 1, get the direction to that hydrogen
+        #  If you have 0, this should have been chosen already
+        if len(my_h) == 1:
+            h1_dir = my_h[0] - pos[oxy]
+        h1_dir /= np.linalg.norm(h1_dir)
+            
+        # Make a bunch of vectors that are orthogonal to the target direction
+        rand_ys = np.random.normal(size=(n_h_guesses, 3))
+        rand_ys = rand_ys / np.linalg.norm(rand_ys, axis=1)[:, None]
+        rand_ys = np.cross(h1_dir, rand_ys)
+        rand_ys = rand_ys / np.linalg.norm(rand_ys, axis=1)[:, None]
+        
+        # Using the plane defined by h1_dir and each new "y", 
+        #  Compute the point that is a 104.5 degree rotation
+        new_vecs = np.cos(_h20_bond_angle) * h1_dir[None, :] + np.sin(_h20_bond_angle) * rand_ys
+        
+        # Compute new coordinates for the H that are 
+        #  0.9641 Ang along that direction
+        possible_placements = 0.9641 * new_vecs + pos[oxy]
+        
+        # Determine distances 
+        #  Shape is: possible_placements x current_atom_count
+        all_dists = np.linalg.norm(pos[None, :, :] - possible_placements[:, None, :], axis=-1)
+        closest_dist = np.min(all_dists, axis=1)  # Shape: possible_placements
+        
+        # Find the farthest atom
+        best_H = np.argmax(closest_dist)
+        
+        # Add it to the list of atoms
+        pos = np.vstack((pos, possible_placements[best_H, :]))
+        
+        # Associate the hydrogen with the oxygen
+        h_map[oxy].append(h_count)
+        h_count += 1
+        
+    # Re-order into OHHOHH format
+    #  We have been keeping track of which Hs are bonded to which other 
+    new_order = np.zeros(pos.shape[:1], dtype=np.int)
+    for i, h_s in enumerate(h_map):  # Loop over each oxygen
+        assert len(h_s) == 2
+        # O's are in the first third. Need to offset index for Hs
+        h_offset = np.add(h_s, len(graph))  
+        new_order[i * 3] = i
+        new_order[i*3+1:i*3+3] = h_offset
+    pos = pos[new_order, :]
+    
+    # Build the ase.Atoms object
+    symbols = ['O', 'H', 'H'] * len(graph)
+    atoms = ase.Atoms(symbols=symbols, positions=pos)
+    
+    return atoms
