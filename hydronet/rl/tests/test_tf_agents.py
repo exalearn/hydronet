e3212475fb75a43538a88678ac8b35292b7af8dc
Logan Ward <ward.logan.t@gmail.com>
2021-07-21 11:54:09 -0400

    Re-implemented the random policy using TF
    Somehow I fixed the issue with matching up to the ReplyBuffer!?

--- hydronet/rl/tests/test_tf_agents.py
+++ hydronet/rl/tests/test_tf_agents.py
@@ -1,16 +1,46 @@
-from hydronet.rl.tf.agents import RandomPolicy
+import numpy as np
+from tf_agents.drivers.dynamic_step_driver import DynamicStepDriver
+from tf_agents.replay_buffers.tf_uniform_replay_buffer import TFUniformReplayBuffer
+from tf_agents.environments.tf_py_environment import TFPyEnvironment
+from tf_agents.specs.tensor_spec import add_outer_dim
+
+from hydronet.rl.tf.agents import ConstrainedRandomPolicy
 from hydronet.rl.tf.env import SimpleEnvironment
 
 
 def test_random():
     env = SimpleEnvironment()
-    policy = RandomPolicy(
-        time_step_spec=env.time_step_spec(),
-        action_spec=env.action_spec()
+    tf_env = TFPyEnvironment(env)
+    policy = ConstrainedRandomPolicy(
+        time_step_spec=add_outer_dim(env.time_step_spec(), tf_env.batch_size),
+        action_spec=add_outer_dim(env.action_spec(), tf_env.batch_size)
     )
 
     # Make an initial step
-    init_step = env.reset()
+    init_step = tf_env.reset()
     allowed_adj = env.get_valid_actions_as_matrix()
     action = policy.action(init_step)
-    assert allowed_adj[action.action[0], action.action[1]] == 1
+    action = np.squeeze(action.action)
+    assert allowed_adj[action[0], action[1]] == 1
+
+
+def test_with_driver():
+    env = SimpleEnvironment()
+    tf_env = TFPyEnvironment(env, check_dims=True)
+    tf_policy = ConstrainedRandomPolicy(
+        time_step_spec=tf_env.time_step_spec(),
+        action_spec=tf_env.action_spec()
+    )
+
+    # Test without buffers
+    driver = DynamicStepDriver(tf_env, tf_policy, [], num_steps=128)
+    init_ts = tf_env.reset()
+    driver.run(init_ts)
+
+    # Test with a buffer
+    buffer = TFUniformReplayBuffer(
+        tf_policy.trajectory_spec,
+        batch_size=tf_env.batch_size
+    )
+    driver = DynamicStepDriver(tf_env, tf_policy, [buffer.add_batch], num_steps=128)
+    driver.run(init_ts)
--- hydronet/rl/tf/agents.py
+++ hydronet/rl/tf/agents.py
@@ -1,32 +1,41 @@
 from typing import Optional
 
-from tf_agents.policies.py_policy import PyPolicy
+from tf_agents.policies import TFPolicy
 from tf_agents.trajectories import time_step as ts, policy_step
 from tf_agents.typing import types
-import numpy as np
+import tensorflow as tf
 
 
-class RandomPolicy(PyPolicy):
+# TODO (wardlt): Sutanay's advice on what to do next:
+#  - Make a version of the environment that gives us an embedding (have the observations be *fingerprints*, etc)
+#  - Explore categorical DQN with embedding features as a baseline
+#  - Focus on making the baselines, scale-ability for the environments
+#     - Note that Reverb (available on GitHub) can work with TF-Agents
 
-    def _action(self, time_step: ts.TimeStep, policy_state: types.NestedArray,
+
+class ConstrainedRandomPolicy(TFPolicy):
+    """Policy that selects a random step from"""
+
+    def _action(self, time_step: ts.TimeStep,
+                policy_state: types.NestedTensor,
                 seed: Optional[types.Seed] = None) -> policy_step.PolicyStep:
         # Get the adj matrix of possible actions
-        allowed_actions_adj = time_step.observation['allowed_actions']
+        assert time_step.observation['allowed_actions'].shape[0] == 1, f'We have not yet implemented batch_size > 1'
+        allowed_actions_adj = time_step.observation['allowed_actions'][0, :, :]
 
         # Transform it to a list of tuples
-        allowed_actions = np.transpose(np.nonzero(allowed_actions_adj))
+        allowed_actions = tf.where(allowed_actions_adj != 0, name='find_allowed_actions')
 
         # Special case: No actions - Return a redo of the first bond (will not change graph
-        if len(allowed_actions) == 0:
+        if tf.size(allowed_actions) == 0:
             return policy_step.PolicyStep(
-                np.array([0, 1], np.int32),
+                tf.constant([[0, 1]], tf.int32),
                 policy_state
             )
 
         # Pick one at random
-        rng = np.random.RandomState(seed)
-        rid = rng.choice(len(allowed_actions))
+        rid = tf.random.uniform(shape=[], maxval=tf.shape(allowed_actions)[0], dtype=tf.int32)
         return policy_step.PolicyStep(
-            np.array(allowed_actions[rid], np.int32),
+            tf.cast([allowed_actions[rid]], tf.int32),
             policy_state
         )
