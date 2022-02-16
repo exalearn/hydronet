4439f7892c6a6da7abfabf688cf74952324e75d7
Logan Ward <ward.logan.t@gmail.com>
2022-02-16 08:25:24 -0500

    Save the network at the end of training

--- challenge-3/rl/run_ppo.py
+++ challenge-3/rl/run_ppo.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser
 from datetime import datetime
 from pathlib import Path
+import pickle as pkl
 import hashlib
 import json
 import csv
@@ -14,9 +15,9 @@ from tqdm import tqdm
 import tensorflow as tf
 import pandas as pd
 
-
+# TODO (wardlt): Do we need this? Could we install the library with `pip install -e` instead?
 import sys
-sys.path.insert(0,'/people/pope044/Exalearn/hydronet')
+sys.path.insert(0, '/people/pope044/Exalearn/hydronet')
 
 from hydronet.rl.tf.networks import GCPNActorNetwork, GCPNCriticNetwork
 from hydronet.rl.tf.env import SimpleEnvironment
@@ -25,8 +26,6 @@ from hydronet.rl.rewards.mpnn import MPNNReward
 from hydronet.rl.rewards.geom import CyclesReward, ASPLReward
 from hydronet.mpnn.layers import custom_objects
 from hydronet.utils import get_platform_info
-
-
 
 # Get the default path for the MPNN
 _mpnn_path = Path() / '..' / '..' / 'challenge-2' / 'best-model' / 'best_model.h5'
@@ -53,7 +52,7 @@ def make_reward(args):
         model = tf.keras.models.load_model(args.mpnn_path, custom_objects=custom_objects)
         reward = CyclesReward()
         second_reward = MPNNReward(model, per_water=True)
-        return reward, second_reward, False 
+        return reward, second_reward, False
     elif args.reward == 'cycles':
         reward = CyclesReward(weight=False)
         return reward, False
@@ -62,7 +61,7 @@ def make_reward(args):
         return reward, False
     else:
         raise ValueError(f'Undefined reward function: {args.reward}')
-        
+
 
 def compute_avg_return(environment, policy, num_episodes=10):
     total_return = 0.0
@@ -113,14 +112,14 @@ def get_trajectories(environment, policy, eng_reward, num_episodes=10) -> pd.Dat
 if __name__ == "__main__":
     # Make an options parser
     arg_parser = ArgumentParser()
-    
+
     #   Group 1: Things related to the environment
     group = arg_parser.add_argument_group('Environment Options', 'Options related to the water cluster environment, such as reward structure')
     group.add_argument('--reward', choices=['mpnn_last', 'mpnn', 'mpnn_combo', 'cycles', 'aspl'],
                        default='mpnn_last', help='Name of the reward function to use. Rewards are defined in code')
     group.add_argument('--mpnn-path', default=str(_mpnn_path), help='Path to the MPNN used for evaluating energy, if needed')
     group.add_argument('--max-size', default=10, help='Maximum size of the water cluster.', type=int)
-    
+
     #   Group 2: Related to the GCPN networks
     group = arg_parser.add_argument_group('GCPN Options', 'Options related to the GCPN networks')
     group.add_argument('--actor-messages', type=int, default=8, help='Number of message passing layers in actor network')
@@ -129,43 +128,43 @@ if __name__ == "__main__":
                                                                            ' generating action probabilities in actor network')
     group.add_argument('--critic-messages', type=int, default=8, help='Number of message passing layers in critic network')
     group.add_argument('--critic-features', type=int, default=64, help='Number of features per node in critic network')
-    
+
     #   Group 3: Related to the PPO learner
     group = arg_parser.add_argument_group('PPO Options', 'Options related to the PPO learner')
     group.add_argument('--ppo-learning-rate', default=1e-3, type=float, help='Learning reate for PPO')
     group.add_argument('--ppo-entropy-regularization', default=1e-4, type=float, help='Entropy regularization for PPO')
     group.add_argument('--ppo-discount', default=1, type=float, help='Discount factor for reward')
-    
+
     #    Group 4: Related to the driver/learner
     group = arg_parser.add_argument_group('Driver Options', 'Options for the driver/learner')
     group.add_argument('--driver-buffer', default=1024, type=int, help='Size of the history buffer')
     group.add_argument('--driver-episodes', default=8, type=int, help='Number of episodes per epoch')
     group.add_argument('--driver-epochs', default=32, type=int, help='Number of epochs')
     group.add_argument('--driver-steps', default=16, type=int, help='Number of training steps per epoch')
-    
+
     #   Group 5: Related to the post-processing
     group = arg_parser.add_argument_group('Post-process Options', 'Options for what to do after training')
     group.add_argument('--post-episode-count', default=128, type=int, help='Number of post-processing episodes to run')
-    
+
     # Parse the arguments
     args = arg_parser.parse_args()
     run_params = args.__dict__
 
     # Get the host information
     host_info = get_platform_info()
-    
+
     # Open an experiment directory
     run_hash = hashlib.sha256(json.dumps(run_params).encode()).hexdigest()[:6]
-    out_dir = Path(f'runs/{args.reward}-W{args.max_size}-{datetime.utcnow().strftime("%d%m%y_%H%M%S")}-{run_hash}')  # TODO (wardlt): Do we want the time in the directory
+    out_dir = Path(f'runs/{args.reward}-W{args.max_size}-{datetime.utcnow().strftime("%y%m%d_%H%M%S")}-{run_hash}')
     out_dir.mkdir(parents=True)
-    
+
     # Save the settings
     run_params['start_time'] = datetime.utcnow().isoformat()
     with open(out_dir / 'run_params.json', 'w') as fp:
-        json.dump(run_params, fp)
+        json.dump(run_params, fp, indent=2)
     with open(out_dir / 'host_info.json', 'w') as fp:
-        json.dump(host_info, fp)
-    
+        json.dump(host_info, fp, indent=2)
+
     # Make the environment
     if 'combo' in args.reward:
         reward, second_reward, only_last = make_reward(args)
@@ -174,13 +173,13 @@ if __name__ == "__main__":
         second_reward = None
     env = SimpleEnvironment(maximum_size=args.max_size, reward=reward, second_reward=second_reward, only_last=only_last)
     tf_env = TFPyEnvironment(env)
-    
+
     # Make a reward for MPNN energy
     model = tf.keras.models.load_model(args.mpnn_path, custom_objects=custom_objects)
     eng_reward = MPNNReward(model, per_water=False)
-    
+
     # Make the GCPN 
-    actor_net = GCPNActorNetwork(tf_env.observation_spec(), tf_env.action_spec(), tf_env.reset(), 
+    actor_net = GCPNActorNetwork(tf_env.observation_spec(), tf_env.action_spec(), tf_env.reset(),
                                  num_messages=args.actor_messages,
                                  node_features=args.actor_features,
                                  graph_features=args.actor_graph_features,
@@ -188,7 +187,7 @@ if __name__ == "__main__":
     critic_net = GCPNCriticNetwork(tf_env.observation_spec(), tf_env.reset(),
                                    num_messages=args.critic_messages,
                                    node_features=args.critic_features)
-    
+
     # Use it to create the PPO agent
     #  We use clipping to avoid problems with exploding KL gradients
     tf_agent = PPOClipAgent(
@@ -202,7 +201,7 @@ if __name__ == "__main__":
         discount_factor=args.ppo_discount,
     )
     tf_agent.initialize()
-    
+
     # Assemble the driver
     #  TODO (wardlt): Keep a priority queue of the best structures
     buffer = TFUniformReplayBuffer(
@@ -210,18 +209,18 @@ if __name__ == "__main__":
         batch_size=1,
         max_length=args.driver_buffer,
     )
-    log_diver = DriverLogger(out_dir / 'training-structures.json')
-    driver = DynamicEpisodeDriver(tf_env, tf_agent.collect_policy, [buffer.add_batch, log_diver], num_episodes=8)
-    
+    log_driver = DriverLogger(out_dir / 'training-structures.json')
+    driver = DynamicEpisodeDriver(tf_env, tf_agent.collect_policy, [buffer.add_batch, log_driver], num_episodes=8)
+
     # Perform an initial test
     agent_return = compute_avg_return(tf_env, tf_agent.collect_policy, 32)
     print(f'Untrained Agent return with randomized policy: {agent_return:.1f}')
-    
+
     # Fill the buffer
     state = tf_env.reset()
     while buffer.num_frames() < buffer.capacity:
         state, _ = driver.run(state)
-        
+
     # Run the training loop
     train_loss = None
     first = True  # Whether we are at the first episode
@@ -245,25 +244,31 @@ if __name__ == "__main__":
             step_info = {'return': avg_return, 'epoch': epoch, 'step': step}
             step_info.update(dict(zip(train_loss.extra._fields, map(float, tuple(train_loss.extra)))))
             step_info['loss'] = train_loss.loss.numpy()
-           
+
             with open(out_dir / 'log.csv', 'a') as fp:
                 writer = csv.DictWriter(fp, fieldnames=step_info.keys())
                 if first:
                     first = False
                     writer.writeheader()
                 writer.writerow(step_info)
-                    
+
             # Update the progress bar
             pbar.set_description(f'loss: {train_loss.loss:.2e} - return: {avg_return:.1f} - step: {step}')
-            
+
     # Record the after-training results
     agent_return = compute_avg_return(tf_env, tf_agent.collect_policy, 32)
     print(f'Trained Agent return with randomized policy: {agent_return:.1f}')
-    
+
     # Save the policy
     tf_policy_saver = PolicySaver(tf_agent.collect_policy, 1)
     tf_policy_saver.save(str(out_dir / 'collect_policy'))
-    
+
+    # Save the actor and critic networks
+    with open(out_dir / 'critic_network.pkl', 'wb') as fp:
+        pkl.dump(critic_net, fp)
+    with open(out_dir / 'actor_network.pkl', 'wb') as fp:
+        pkl.dump(actor_net, fp)
+
     # Do the post-processing
     rl_traj = get_trajectories(tf_env, tf_agent.collect_policy, eng_reward, args.post_episode_count)
     rl_traj.to_pickle(out_dir / 'final_trajs.pkl')
